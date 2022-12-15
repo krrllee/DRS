3d51951d71c2a105bfac92eb9df573925d2d2919
krrllee <krleposkok12@gmail.com>
2022-12-15 22:16:59 +0100

    debug

---
+++ frontend/react-drs/src/pages/LandingPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const LandingPage = () =>{
+    return(
+        <div>
+            <a href="/login">Login</a>
+            <br/>
+            <br/>
+            <a href="/register">Register</a>
+        </div>
+    );
+};
+
+export default LandingPage;
