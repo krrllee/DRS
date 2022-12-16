dbf0713128ec9dc23e13a753ef1c5a154115ae7d
Sibirjak13 <119792795+Sibirjak13@users.noreply.github.com>
2022-12-16 15:01:37 +0100

    prepravljena baza

--- backend/db/users.sql
+++ backend/db/users.sql
@@ -1,18 +1,22 @@
 -- Adminer 4.8.1 PostgreSQL 15.1 (Debian 15.1-1.pgdg110+1) dump
 
 DROP TABLE IF EXISTS "users";
+DROP SEQUENCE IF EXISTS users_id_seq;
+CREATE SEQUENCE users_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
+
 CREATE TABLE "public"."users" (
-    "username" character varying(20) NOT NULL,
-    "first_name" character varying(20) NOT NULL,
-    "last_name" character varying(20) NOT NULL,
-    "adress" character varying(50) NOT NULL,
-    "city" character varying(20) NOT NULL,
-    "state" character varying(20) NOT NULL,
-    "phone_number" character varying(20) NOT NULL,
-    "email" character varying(50) NOT NULL,
-    "password" character varying(20) NOT NULL,
-    CONSTRAINT "radnik_pk" PRIMARY KEY ("username")
+    "id" integer DEFAULT nextval('users_id_seq') NOT NULL,
+    "username" character varying(100) NOT NULL,
+    "first_name" character varying(100) NOT NULL,
+    "last_name" character varying(100) NOT NULL,
+    "adress" character varying(100) NOT NULL,
+    "city" character varying(100) NOT NULL,
+    "state" character varying(100) NOT NULL,
+    "phone_number" character varying(100) NOT NULL,
+    "email" character varying(100) NOT NULL,
+    "password" character varying(100) NOT NULL,
+    CONSTRAINT "radnik_pk" PRIMARY KEY ("id")
 ) WITH (oids = false);
 
 
--- 2022-12-14 16:53:52.384973+00
+-- 2022-12-16 13:35:30.644536+00
